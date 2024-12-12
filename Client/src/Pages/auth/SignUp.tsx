14fad3d6cc44cf98d72d36bc52aaeb2d90c093cb
Wosmos <m.wasifmalik17@gmail.com>
2024-12-12 10:03:00 +0500

    client reinitialized

--- Client/src/Pages/auth/SignUp.tsx
+++ Client/src/Pages/auth/SignUp.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import Img from "../../assets/Top.png";
-import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi"; // Import icons from react-icons
-
-
-
+import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi"; 
 
 
 const SignUp = () => {
